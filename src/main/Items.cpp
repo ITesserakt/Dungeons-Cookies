d680e5454699d79be65cbf220ad432d289eaa12b
Lokson1 <62993386+Lokson1@users.noreply.github.com>
2020-10-23 21:21:49 +0300

    Добавил короткое имя и описание

--- src/main/Items.cpp
+++ src/main/Items.cpp
@@ -1,6 +1,10 @@
-
+#include <string>
 
 class Item
 {
-    ;
+    private:
+        // Описание конкретного предмета
+        std::string description;
+        // Чтобы не делать разбор падежей, будем использовать "корень" слова. Например для ложки - ложк
+        std::string short_name;
 };
